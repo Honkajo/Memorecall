4f184f9a9e12e06c77ac0118b4d56322ab432fb9
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-02-18 22:11:12 +0200

    Added requirements.txt and made changes to app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.sql import text
 from os import getenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
